b99a2bcf68c178de066f1e69f433762aaab36cd7
ados1984 <ados1984@gmail.com>
2015-04-08 22:50:47 -0400

    Initial Commit

---
+++ src/main/java/com/mycart/controllers/home/HomeServlet.java
@@ -0,0 +1,34 @@
+package com.mycart.controllers.home;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	public void init() throws ServletException {
+		message = "Hello World";
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message +"</h1>");
+	}
+
+	public void destory() {
+
+	}
+}
