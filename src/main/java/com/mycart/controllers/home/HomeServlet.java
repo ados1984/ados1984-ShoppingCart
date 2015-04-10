e9883933773c8436187dac746259b26bbbbff4a4
ados1984 <ados1984@gmail.com>
2015-04-09 23:37:17 -0400

    Trying out Request Dispatcher

--- src/main/java/com/mycart/controllers/home/HomeServlet.java
+++ src/main/java/com/mycart/controllers/home/HomeServlet.java
@@ -3,6 +3,7 @@ package com.mycart.controllers.home;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,15 +19,20 @@ public class HomeServlet extends HttpServlet {
 	private String message;
 
 	public void init() throws ServletException {
-		message = "Hello World";
+		//message = "Hello World";
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message +"</h1>");
+		System.out.println("Request Scheme: "+request.getScheme());
+		System.out.println("Request ServerName: "+request.getServerName());
+		System.out.println("Request ServerPort: "+request.getServerPort());
+		System.out.println("Request URI: "+request.getRequestURI());
+		System.out.println("Request QueryString: "+request.getQueryString());
+		request.getQueryString();
+		
 	}
+	
 
 	public void destory() {
 
